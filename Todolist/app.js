ecf2c84a76dcd39a0c279fbe649fb2fe9fb754d1
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-07 12:50:43 +0530

    Project : To do list

--- Todolist/app.js
+++ Todolist/app.js
@@ -3,28 +3,26 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
+var items=["Exercise","Breakfast"];
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",function(req,res){
     var today=new Date();
-    var currentDay=today.getDay();
-    var day="";
-    // if(currentDay==6||currentDay==0){
-    //   day="weekend";
-    // }
-    // else{
-    //   day="working day";
-  //}
-  if(currentDay==0){day="Sunday";}
-  else if (currentDay==1) {day="Monday";}
-  else if (currentDay==2) {day="Tuesday";}
-  else if (currentDay==3) {day="Wedday";}
-  else if (currentDay==4) {day="Thursday";}
-  else if (currentDay==5) {day="Friday";}
-  else day="Saturady";
+    var options={
+      weekday:"long",
+      day:"numeric",
+      month:"long"
+    };
+    var day=today.toLocaleDateString("en-US",options);
 
-  res.render("List",{kindOfDay:day});
+  res.render("list",{kindOfDay:day,newListItems:items});
+});
+app.post("/",function(request,response){
+  var item=request.body.newItem;
+  items.push(item);
+  response.redirect("/");
 });
 
 app.listen(3000,function(){
